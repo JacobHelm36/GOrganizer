20bf5960d415bfffbe2ccbd53643acababe8238d
jhelm3 <jakehelm36@gmail.com>
2020-03-02 14:30:34 -0700

    changed the SASS colors, set-up the AUTH0

--- client/src/authConfig.js
+++ client/src/authConfig.js
@@ -1,3 +1,3 @@
-export const domain = "";
-export const audience = "";
-export const clientId = "";
+export const domain = "dev-sq-bjbln.auth0.com";
+export const audience = "https://gorganizer.com";
+export const clientId = "pl1KqEhqhFC9au0m3OHkZpUF1580qMrL";

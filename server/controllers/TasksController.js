0f6e1f0afd5726e5c44ce24a0f3570401b5caf64
devingrayt6 <devingrayt6@gmail.com>
2020-03-02 16:06:27 -0700

    Tasks routes initial commit

---
+++ server/controllers/TasksController.js
@@ -0,0 +1,52 @@
+import express from "express";
+import BaseController from "../utils/BaseController";
+import { tasksService } from "../services/TasksService";
+import auth0provider from "@bcwdev/auth0provider";
+
+export class TasksController extends BaseController {
+  constructor() {
+    super("api/tasks");
+    this.router = express
+      .Router()
+      .get("", this.getAll)
+      // NOTE: Beyond this point all routes require Authorization tokens (the user must be logged in)
+      .use(auth0provider.isAuthorized)
+      .post("", this.create)
+      .put("/:id", this.edit)
+      .delete("/:id", this.delete)
+  }
+  async getAll(req, res, next) {
+    try {
+      let data = await tasksService.getAll(req.body.id);
+      return res.send(data)
+    } catch (error) {
+      next(error);
+    }
+  }
+  async create(req, res, next) {
+    try {
+      // NOTE NEVER TRUST THE CLIENT TO ADD THE CREATOR ID
+      req.body.creatorId = req.user.sub;
+      res.send(req.body);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async edit(req, res, next) {
+    try {
+      let data = await tasksService.edit(req.params.id, req.userInfo.email, req.body);
+      res.send(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  async delete(req, res, next) {
+    try {
+      let data = await tasksService.delete(req.params.id, req.userInfo.email);
+      res.send(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 import ValueSchema from "../models/Value";
 import ProfileSchema from "../models/Profile";
-import BoardSchema from '../models/Board'
+import BoardSchema from '../models/Board';
+import TaskSchema from '../models/Task';
 class DbContext {
   Values = mongoose.model("Value", ValueSchema);
   Profile = mongoose.model("Profile", ProfileSchema);
-  Boards = mongoose.model("Board", BoardSchema)
+  Boards = mongoose.model("Board", BoardSchema);
+  Tasks = mongoose.model("Task", TaskSchema);
 }
 
 export const dbContext = new DbContext();

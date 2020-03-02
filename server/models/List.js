2f95a310f1bdf55ae997c8a291f2b7ba92ee6eba
jhelm3 <jakehelm36@gmail.com>
2020-03-02 15:55:41 -0700

    got the lists routes working

--- server/models/List.js
+++ server/models/List.js
@@ -3,8 +3,7 @@ let Schema = mongoose.Schema
 let ObjectId = Schema.Types.ObjectId
 
 const List = new Schema({
-  title: { type: String, required: true },
-  creatorEmail: { type: String, required: true },
+  title: { type: String, required: true},
   boardId: { type: ObjectId, ref: 'Board', required: true }
 }, { timestamps: true, toJSON: { virtuals: true } })
 
